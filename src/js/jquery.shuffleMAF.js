ff66708b118e25835769f9ae8405635bc2795270
mohssineAboutaj <mohssineaboutaj1995@gmail.com>
2019-02-11 18:12:34 +0100

    some changes

--- src/js/jquery.shuffleMAF.js
+++ src/js/jquery.shuffleMAF.js
@@ -8,41 +8,37 @@
 */
 
 (function ( $ ) {
-  $.fn.shuffleMAF = function(e) {
+  $.fn.shuffleMAF = function(options) {
 
     "use strict";
 
-    this.each(function(){
+    return this.each(function(){
 
-      var shuffleMAF = this;
+      var shuffleMAF = $(this);
 
-      // define shortHand if...else variables
-      var myEffect,
-          time,
-          classActive;
+      // define default options
+      var settings = $.extend({
+        effect: 'fade',
+        time: 700,
+        classActive: 'shuffle-active'
+      }, options);
 
-      // the main effect if not defined the default effect is fadeIn/fadeOut
-  //    myEffect = void 0 === $(this).attr('data-shuffleMAF-effect') ? "fade" : $(this).attr('data-shuffleMAF-effect');
-      myEffect = void 0 === e.effect ? "fade" : e.effect;
-
-      // active class name
-        classActive = void 0 === e.active ? "shuffle-active" : e.active;
-      classActive = "shuffle-active";
-
-      // main dyration to do the effect
-        time = void 0 === e.time ? 700 : e.time;
-  //    time = void 0 === $(this).attr('data-shuffleMAF-time') ? 700 : $(this).attr('data-shuffleMAF-timz');
+      console.log(settings);
 
       // on click event
       shuffleMAF + $(" .shuffle-nav li").on("click", function() {
+
+
+/*
+
         // add class active on this and remove from others
         $(this).addClass(classActive).siblings().removeClass(classActive);
 
         // check if this is first btn
         if ($(this).is(":first-child")) {
-          if (myEffect === "fade") {
+          if (effect === "fade") {
             shuffleMAF + $(" .shuffle-content .shuffle-item").fadeIn(time);
-          } else if (myEffect === "opacity") {
+          } else if (effect === "opacity") {
             shuffleMAF + $(" .shuffle-content .shuffle-item").animate({
               opacity: 1
             }, time);
@@ -50,10 +46,10 @@
             shuffleMAF + $(" .shuffle-content .shuffle-item").fadeIn(time)
           }
         } else { // check if this is not first btn
-          if (myEffect === "fade") {
+          if (effect === "fade") {
             shuffleMAF + $(" .shuffle-content .shuffle-item").fadeOut(time);
             shuffleMAF + $(" .shuffle-content .shuffle-item." + $(this).data("filter")).fadeIn(time);
-          } else if (myEffect === "opacity") {
+          } else if (effect === "opacity") {
             shuffleMAF + $(" .shuffle-content .shuffle-item").animate({
               opacity: .1
             }, time);
@@ -65,8 +61,10 @@
             shuffleMAF + $(" .shuffle-content .shuffle-item." + $(this).data("filter")).fadeIn(time);
           }
         }
+
+*/
+
       });
     });
-    return this;
   }
 }( jQuery ));
